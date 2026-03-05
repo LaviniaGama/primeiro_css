613fba1c5caed68c10ae345980b7cb69b70f174d
LaviniaGama <laviniaemanuellegama@gmail.com>
2026-03-05 14:50:48 -0300

    correção

--- script.js
+++ script.js
@@ -1,17 +1,81 @@
 function somar() {
-    let num;
-    let numl;
-    num = parseInt(document.getFlementById('1').value)
-    numl = parseInt(document.getFlementById('2').value)
-    document.getFlementById('resultado').InnerText = num + numl
-    console.log(num+numl)
+    let num
+    let num1
+    num = parseInt(document.getElementById('1').value)
+    num1 = parseInt(document.getElementById('2').value)
+    document.getElementById("./resultado").innerText = num+num1
+    console.log(num + num1)
 }
+ function multiplicar(){
+    let num2 
+    let num3
+     num3 = parseInt(document.getElementById('3').value)
+     num4 = parseInt(document.getElementById('4').value)
+      document.getElementById("./resultado2").innerText = num2 * num3
+      console.log(num2 * num3)
+ }
+ function subtrair(){
+    let num4
+    let num5
+     num4 = parseInt(document.getElementById('5').value)
+     num5 = parseInt(document.getElementById('6').value)
+      document.getElementById("./resultado3").innerText = num4 - num5
+      console.log(num4 - num5)
+ }
 
-function subtrair() {
-    let num;
-    let numl;
-    num = parseInt(document.getFlementById('1').value)
-    numl = parseInt(document.getFlementById('2').value)
-    document.getFlementById('resultado').InnerText = num - numl
-    console.log(num-numl)
-}
+ function area(base, altura){
+    let num6
+    let num7
+     num6 = parseInt(document.getElementById('7').value)
+     num7 = parseInt(document.getElementById('8').value)
+      document.getElementById("./resultado4").innerText = num6 * num7/ 2
+      console.log(num6 - num7/2)
+ }
+  function quadrado(){
+    let num8
+   
+     num8 = parseInt(document.getElementById('9').value)
+      document.getElementById("./resultado5").innerText = num8 * num8
+      console.log(num8 * num8)
+ }
+  
+function retangulo(){
+    let num9
+    let num10
+     num9 = parseInt(document.getElementById('10').value)
+     num10 = parseInt(document.getElementById('11').value)
+      document.getElementById("./resultado6").innerText = num9 * num10
+      console.log(num9 * num10)
+ }
+
+ function trapezio(){
+    let num11
+    let num12
+    let num13
+     num11 = parseInt(document.getElementById('12').value)
+     num12 = parseInt(document.getElementById('13').value)
+      num13 = parseInt(document.getElementById('14').value)
+      document.getElementById("./resultado7").innerText = num11 + num12 *num13/2
+      console.log(num11 + num12 * num13/2)
+ }
+
+ function losangulo(){
+    let num14
+    let num15
+    let num16
+     num14 = parseInt(document.getElementById('15').value)
+     num15 = parseInt(document.getElementById('16').value)
+      num16 = parseInt(document.getElementById('17').value)
+      document.getElementById("./resultado8").innerText = num14 + num15 *num16/2
+      console.log(num14 + num15 * num16/2)
+ }
+ function losangulo(){
+    let num14
+    let num15
+    let num16
+     num14 = parseInt(document.getElementById('15').value)
+     num15 = parseInt(document.getElementById('16').value)
+      num16 = parseInt(document.getElementById('17').value)
+      document.getElementById("./resultado8").innerText = num14 + num15 *num16/2
+      console.log(num14 + num15 * num16/2)
+ }
